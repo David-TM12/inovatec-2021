c10e1f4183b77775e39c2d99aff797cdb3b12eec
DavidTM <deivi_gf144@hotmail.com>
2021-05-23 23:48:37 -0400

    fix nome equipe

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,7 @@ function Footer(){
        
     <footer className="footer mt-auto py-3 bg-dark">
         <div className="container">
-            <p className="text-light">App desenvolvido por <a href="https://github.com/David-TM12/inovatec-2021" target="_blank" rel="noreferrer">Equipe 3</a></p>
+            <p className="text-light">App desenvolvido por <a href="https://github.com/David-TM12/inovatec-2021" target="_blank" rel="noreferrer">Equipe 1</a></p>
             <p className="text-light"><small><strong>INOVATEC-2021</strong><br/> Leitos de manaus - projeto desenvolvido pelos alunos da famentro do curso de sistemas de informação para o INOVATEC-2021</small></p>
         </div>
     </footer>
